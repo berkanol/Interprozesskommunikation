dde473cb20acf8ab5e0b5993171bce0c49bc0f0b
berkanol <104497238+berkanol@users.noreply.github.com>
2022-05-03 15:47:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# Importieren von Pygame Paketen
+import pygame
+import sys
+import string
+
+# Funktionen für das Grundgerüst damit Pygame laufen kann
+pygame.init()
+bildschirm = pygame.display.set_mode([1000, 550])
+pygame.display.set_caption("Schiffe versenken")
+hintergrund = pygame.image.load("Grafiken-SchiffeVersenken/hintergrund.jpg")
+
+# Variablen
+spielbrettbreite = 200
+spielbretthoehe = 200
+kästchengroesse = 40
+blau = (0, 0, 255)
+grün = (0, 255, 0)
+rot = (255, 0, 0)
+grau = (190, 190, 190)
+schwarz = (0, 0, 0)
+ABC_G = string.ascii_uppercase
+
+
+def spielfeld_erstellen():
+    bildschirm.blit(hintergrund, (0, 0))
+
+    pygame.display.update()
+
+
+go = True
+while go:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    spielfeld_erstellen()
