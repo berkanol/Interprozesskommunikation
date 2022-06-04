1552d34abd6215daa570fdba483ca51e3f9f02ab
berkanol <104497238+berkanol@users.noreply.github.com>
2022-06-04 19:17:34 +0200

    Update pipes.c

--- pipes.c
+++ pipes.c
@@ -13,53 +13,97 @@
 #include <time.h>
 
 int main(int argc, char* argv[]) {
-    int fd[2];
-    if (pipe(fd) == -1) {
+    int fd[3][2];
+    int i;
+    for (i = 0; i < 3; i++) {
+        if (pipe(fd[i]) < 0) {
             return 1;
         }
+    }
     
-    int id1 = fork();
-            if (id1 == 0) {
-                close(fd[0]);
+    int pid1 = fork();
+    if (pid1 < 0) {
+        return 2;
+    }
+    
+    if (pid1 == 0) {
+        // Child process 1
+        close(fd[0][1]);
+        close(fd[1][0]);
+        close(fd[2][0]);
+        close(fd[2][1]);
+        int i;
+        int arr[10];
+        srand(time(NULL));
+        
+        for (i = 0; i < 10; i++) {
+            arr[i] = rand() % 11;
+            
+        }
+                        
+        if (write(fd[1][1], &arr, sizeof(int) * 10) < 0) {
+            return 4;
+            
+        }
+        close(fd[0][0]);
+        close(fd[1][1]);
+        printf("Ich bin conv\n");
+    }
+    
+    int pid2 = fork();
+    if (pid2 < 0) {
+        return 5;
+    }
+    
+    if (pid2 == 0) {
+        // Child process 2
+        close(fd[0][0]);
+        close(fd[0][1]);
+        close(fd[1][1]);
+        close(fd[2][0]);
+        int arr[10];
+        int length = sizeof(arr)/sizeof(arr[0]);
                 
-                int i;
-                int arr[10];
-                
-                srand(time(NULL));
-                
-                printf("Generated: ");
-                for (i = 0; i < 10; i++) {
-                    arr[i] = rand() % 11;
-                    printf("%d ", arr[i]);
-                }
-                printf("\n");
-                
-                if (write(fd[1], &arr, sizeof(int) * 10) < 0) {
-                    return 4;
-                }
-                
-                close(fd[1]);
-                printf("Ich bin conv\n");
-            } else {
-                close(fd[1]);
-                int arr[10];
-                int length = sizeof(arr)/sizeof(arr[0]);
-                
-                if (read(fd[0], &arr, sizeof(int) * 10) < 0) {
-                    return 6;
-                }
-                
-                read(fd[0], &arr, sizeof(int) * 10);
-                close(fd[0]);
-                
-                for (int i = 0; i < length; i++) {
-                       printf("%d ", arr[i]);
-                }
-                wait(NULL);
-                printf("\n");
-                printf("Ich bin log\n");
+        if (read(fd[1][0], &arr, sizeof(int) * 10) < 0) {
+            return 6;
+            
+        }
+                       
+        read(fd[1][0], &arr, sizeof(int) * 10);
+        close(fd[1][0]);
+        close(fd[2][1]);
+        
+        printf("Generated: ");
+        for (int i = 0; i < length; i++) {
+            printf("%d ", arr[i]);
+            
+        }
+        printf("\n");
+        printf("Ich bin log\n");
+    }
+    
+    // Parent process
+    close(fd[0][0]);
+    close(fd[1][0]);
+    close(fd[1][1]);
+    close(fd[2][1]);
+    
+    int sum = 0;
+    int arr [10];
+
+    read(fd[2][0], &arr, sizeof(int));
+    
+    close(fd[0][1]);
+    close(fd[2][0]);
+    
+    for (i = 0; i < 10; i++) {
+                sum += arr[i];
             }
-   
     
+    printf("Result is %d\n", sum);
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
     return 0;
 }
