287c8bf8b3687a73af821a179eacc137c47e00d1
berkanol <104497238+berkanol@users.noreply.github.com>
2022-06-13 16:27:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,29 +9,34 @@
 #include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    int fd[2];
     int id1 = fork();
-
-    if (id1 == 0) {
-        int id2 = fork();
-        if (id2 == 0) {
-            int id3 = fork();
-            if (id3 == 0) {
-                printf("Ich bin conv\n");
-            } else {
-                wait(NULL);
-                printf("Ich bin log\n");
-            }
-        } else {
-            wait(NULL);
-            printf("Ich bin stat\n");
+    
+    if (pipe(fd) == -1) {
+            printf("An error ocurred with opening the pipe\n");
+            return 1;
         }
-    } else {
-        wait(NULL);
-        printf("Ich bin report\n");
-    }
     
-    
-    
+    if (id1 == 0) {
+           int id2 = fork();
+           if (id2 == 0) {
+               int id3 = fork();
+               if (id3 == 0) {
+                   int id4 = fork();
+                   if (id4 == 0) {
+                       printf("CONV\n");
+                   } else {
+                       wait(NULL);
+                       printf("LOG\n");
+                   }
+               } else {
+                   wait(NULL);
+                   printf("STAT\n");
+               }
+           } else {
+               wait(NULL);
+               printf("REPORT\n");
+       }
     return 0;
 }
