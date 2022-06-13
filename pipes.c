03fe21afb1ea9a98b113e0e4c71681af1171d5b3
berkanol <104497238+berkanol@users.noreply.github.com>
2022-06-13 12:34:41 +0200

    Update pipes.c

--- pipes.c
+++ pipes.c
@@ -15,144 +15,148 @@
 #include <sys/fcntl.h>
 
 int main(int argc, char* argv[]) {
-    int fd[2];
     
-    if(mkfifo("testfifo" ,0666)< 0) {
-   printf("Das Anlegen der benannten Pipe ist fehlgeschlagen.\n");
-    } else {
-   printf("Die benannte Pipe testfifo wurde angelegt.\n");
-   }
+    int id1 = fork();                           // Prozess 1 wird erschaffen durch den Befehl fork()
+    int fd1 [2];                               // Pipes werden erstellt
     
-    
-    
-    
-    if (pipe(fd) == -1) {
+    // Pipe wird geöffnet und vorgehen bei Fehler wird definiert
+    if (pipe(fd1) == -1) {
+        printf("Pipe konnte nicht geöffnet werden");
         return 1;
     }
     
-    int pid = fork();
-    if (pid == -1) {
-        return 2;
-    }
+    // Bennante Pipe wird erstellt
+    if(mkfifo("testfifo" ,0666)< 0) {
+       printf("Das Anlegen der benannten Pipe ist fehlgeschlagen.\n");
+        } else {
+       printf("Die benannte Pipe testfifo wurde angelegt.\n");
+       }
     
-    if (pid == 0) {
-        // Conv
-        close(fd[0]);
-        
-        int i;
-        int arr[10];
-        
-        srand(time(NULL));
-        int n = 10;
-        
-        printf("Generiert in CONV: ");
-        for (i = 0; i < n; i++) {
-            arr[i] = rand() % 11;
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
-        
-        
-        if (write(fd[1], &arr, sizeof(int) * n) < 0) {
-            return 3;
-        }
-
-        close(fd[1]);
-    } else {
-        // log
-        wait(NULL);
-        close(fd[1]);
-        
-        int arr[10];
-        int i;
-        int n = 10;
-        
-        
-        if (read(fd[0], &arr, sizeof(int) * n) < 0) {
-            return 4;
-        }
-        
-        printf("Übergeben wurde in LOG: ");
-        close(fd[0]);
-        for (i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
-        
-        int fddatei = open("testfifo", O_WRONLY);
-            if (fddatei == -1) {
-                return 5;
-            }
-            
-            if (write(fddatei, &arr, sizeof(int) * n) == -1) {
-                return 6;
-            }
-            
-            close(fddatei);
-        
-    }
-
-        int fd2 [2];
-        int pid2 = fork();
-        
-        if (pid2 == -1) {
-            return 7;
-        }
-        
-        if (pid2 == 0) {
-            // stat
-            waitpid(pid, NULL, 0);
-            close(fd2[0]);
-            
-            
-            int arr[10];
-            int n = 10;
-            int fddatei = open("testfifo", O_RDONLY);
-            if (fddatei == -1) {
-                return 7;
-            }
+    
+    // Erstellen und Ablauf aller 4 Prozesse (CONV, LOG, STAT, REPORT) und der Datenaustausch mithilfe von Pipes
+    if (id1 == 0) {
+        int id2 = fork();                                   // Prozess 2
+        if (id2 == 0) {
+            int id3 = fork();                               // Prozess 3
+            if (id3 == 0) {
                 
-            int i;
-            
-            printf("Received in STAT: ");
-            for (i = 0; i < n; i++) {
+                // CONV
+                
+                close(fd1[0]);                              // Pipe zum Lesen wird geschlossen
+                
+                int i;
+                int arr[10];
+                        
+                srand(time(NULL));
+                int n = 10;
+                
+                // For-Schleife zum generieren von Zufälligen Zahlen
+                printf("Generiert in CONV: ");
+                for (i = 0; i < n; i++) {
+                    arr[i] = rand() % 11;
+                    printf("%d ", arr[i]);
+                }
+                printf("\n");
+                
+                // In die Pipe zum Schreiben wird geschrieben
+                write(fd1[1], &arr, sizeof(int) * n);
+                
+                if (write(fd1[1], &arr, sizeof(int) * n) < 0) {
+                    return 2;
+                }
+                
+                close(fd1[1]);                              // Pipe zum Schreiben wird geschlossen
+            } else {
+                
+                // LOG
+                
+                wait(NULL);                                 // Wartet bis der Kindprozess (CONV) sich beendet
+                        
+                int arr[10];
+                int i;
+                int n = 10;
+                
+                // Liest die Daten die vom Kindprozess (CONV) geschrieben wurden und fügt Sie in ein Array ein
+                if (read(fd1[0], &arr, sizeof(int) * n) < 0) {
+                    return 3;
+                }
+                
+                
+                // Benannte Pipes
+                int fddatei = open("testfifo", O_RDWR);
+                if (fddatei == -1) {
+                    return 5;
+                }
+                            
+                if (write(fddatei, &arr, sizeof(int) * n) == -1) {
+                    return 6;
+                }
+                
+                // Überprüfen ob alle Zahlen richtig übergeben wurden
+                printf("Erhalten in LOG: ");
+                for (i = 0; i < n; i++) {
                 if (read(fddatei, &arr[i], sizeof(int)) == -1) {
                     return 8;
+                    }
+                printf("%d ", arr[i]);
                 }
+                printf("\n");
+                
+                close(fddatei);
+                close(fd1[0]);
+                
+                if (write(fd1[1], &arr, sizeof(int) * n) < 0) {
+                    return 4;
+                }
+                
+                close(fd1[1]);
+            }
+        } else {
+            
+            // STAT
+            
+            wait(NULL);
+            
+            int arr[10];
+            int i;
+            int n = 10;
+            
+                
+            if (read(fd1[0], &arr, sizeof(int) * n) < 0) {
+                return 5;
+            }
+    
+            close(fd1[0]);
+            printf("Erhalten in STAT: ");
+            for (i = 0; i < n; i++) {
                 printf("%d ", arr[i]);
             }
-
-           
+            printf("\n");
             int sum = 0;
-                for (i = 0; i < n; i++) {
-                    sum += arr[i];
-                }
-            
-            printf("Summe ist %d\n", sum);
-            if (write(fd2[1], &sum, sizeof(int)) < 0) {
-                return 9;
+            for (i = 0; i < n; i++) {
+                sum += arr[i];
             }
+            printf("Summe ist: %d\n", sum);
+            write(fd1[1], &sum, sizeof(sum));
             
-            close(fddatei);
-            close(fd2[1]);
+            close(fd1[1]);
             
-            
-            
-        } else {
-            // report
-            close(fd2[1]);
-            int sum = 0;
-            
-            
-            if (read(fd2[0], &sum, sizeof(int)) < 0) {
-                return 10;
-            }
-            printf("\nsumme ist: %d ", sum);
-            
-            
-            close(fd2[0]);
         }
-   
-    return 0;
+    } else {
+        
+        // REPORT
+        
+        wait(NULL);
+        close(fd1[1]);
+        
+        int sum ;
+        
+        read(fd1[0], &sum, sizeof(sum));
+        
+        printf("Erhalten in REPORT: %d\n", sum);
+        close(fd1[0]);
+       
     }
-
+    
+    return 0;
+}
