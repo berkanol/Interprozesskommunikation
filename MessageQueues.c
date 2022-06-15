fd35904ef6f2603c18beaf08150df251a57d88ed
SnoopySama <104512094+SnoopySama@users.noreply.github.com>
2022-06-15 19:00:28 +0200

    Update MessageQueues.c

--- MessageQueues.c
+++ MessageQueues.c
@@ -1,87 +1,72 @@
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <stdio.h>
-#include <sys/msg.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
 #include <time.h>
+#include <sys/msg.h>
 
+//buffer anlegen
 struct msgbuffer{
     long msgtype;
-    char msgtext[100];
+    char msgtext [100];
     int arr[100];
 }msg;
 
-int main(int argc, const char ** argv){
-    struct msgbuffer msg;
+int main(int argc , char ** argv){
+    //benötigte variablen
+    int returncode_msgget;
+    int returncode_msgrcv;
     int i;
-    int n = 10;
-    int returncode_msgget, returncode_msgrcv;
-    returncode_msgget = msgget(1234, 0600| IPC_CREAT);
     
-    int id1 = fork();
-    if (id1 == 0) {
-        int id2 = fork();
-        if (id2 == 0) {
-            int id3 = fork();
-            if (id3 == 0) {
-                int id4 = fork();
-                if (id4 == 0) {
-                    srand(time(NULL));
-                    for (i = 0; i < n; i++) {
-                        msg.arr[i] = rand() %101;
-                        printf("%d,", msg.arr[i]);
-                    }
-                }else {
-                    wait(NULL);
-//                    int i = 0;
-//                    int arr [100];
-                    strcpy(msg.msgtext, "test");
-                    printf("be\n");
-//                    int datei = fputs("ich", &msg);
-                    printf("hallo");
-                    if (msgsnd(returncode_msgget, &msg, sizeof(msg.msgtext), 0) == -1){
-                        printf("message not sent\n");
-                        perror("msgsnd()");
-//                        close(datei);
-                    }else{
-                        printf("kaka");
-                    }
-                    printf("Ich bin Conv\n");
-                    
-                }
-            }else {
-                wait(NULL);
-                struct msgbuffer{
-                    long mtype;
-                    char mtext [100];
-                } msg;
-                
-//                int i = 0;
-//                struct msgbuffer mq;
-//                msg receivebuffer;
-                msg.mtype = 1;
-                if ((returncode_msgget = msgget(1234, 0600)) < 0) {
-                    printf("could not reach Message queue\n");
-                    perror("msgget()");
-                }
-                returncode_msgrcv = msgrcv(returncode_msgget, &msg, sizeof(msg.mtext), msg.mtype, MSG_NOERROR|IPC_NOWAIT);
-                if (returncode_msgrcv == -1) {
-                    printf("message not received\n");
-                    perror("msgrcv()");
-                
-                }
-//                printf("%s\n", msg.mtext);
-                printf("Ich bin log\n");
-            }
-        }else {
-            wait(NULL);
-            printf("ich bin stat\n");
-        }
-    }else {
-        wait(NULL);
-        printf("ich bin report\n");
+    //message queue anlegen
+    returncode_msgget = msgget(1234, IPC_CREAT |0600);
+    
+    //for schleife um zufallszahlen zu generieren
+    srand(time(NULL));
+    for (i = 0; i < 10; i++) {
+        msg.arr[i] = rand() %101;
+        printf("%d,", msg.arr[i]);
     }
+    //message senden
+    msg.msgtype = 1;
+    strcpy(msg.msgtext,"test");
+//    int datei = fputs("ich", &msg);
+    
+    //if schleife zur überprüfung, ob nachricht abgeschickt wurde
+    if (msgsnd(returncode_msgget, &msg, sizeof(msg.msgtext), 0) == -1){
+        printf("message not sent\n");
+        perror("msgsnd()");
+    }
+    //buffer für message receiver
+    wait(NULL);
+    struct msgbuffer{
+        long mtype;
+        char mtext [100];
+    } msg;
+    
+    // auf bereits erstellten message queue zugreifen
+    // überprüfung ob auf message queue zugegriffen werden kann
+    msg.mtype = 1;
+    if ((returncode_msgget = msgget(1234, IPC_CREAT| 0600)) < 0) {
+        printf("could not reach Message queue\n");
+        perror("msgget()");
+    }
+    //überprüfen ob nachricht angekommen ist
+    returncode_msgrcv = msgrcv(returncode_msgget, &msg, sizeof(msg.mtext), msg.mtype, MSG_NOERROR|IPC_NOWAIT);
+    if (returncode_msgrcv == -1) {
+        printf("message not received\n");
+        perror("msgrcv()");
+    //wenn message angekommen ist, soll message angezeigt werden
+    }else {
+         printf ("Diese Nachricht wurde aus der Warteschlange gelesen : %s \n" , msg . mtext );
+         printf ("Die empfangene Nachricht ist %i Zeichen lang .\n" , returncode_msgrcv );
+         }
+//    printf("Drei");
 }
+
 
